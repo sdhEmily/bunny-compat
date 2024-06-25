8375b5cabf6bdc362c75aa2d9867a21977643685
sdhEmily <143989797+sdhEmily@users.noreply.github.com>
2024-06-25 13:23:47 +0100

    bunny ify

--- src/index.tsx
+++ src/index.tsx
@@ -22,7 +22,7 @@ const VendettaCompat: Plugin = {
       }
 
       // basedetta urldetta odetta vendetta
-      const url = "https://raw.githubusercontent.com/vendetta-mod/builds/master/vendetta.js"
+      const url = "https://raw.githubusercontent.com/pyoncord/bunny-builds/main/bunny.js"
 
       // gedetta thedetta vendetta codetta asdetta textdetta
       const res = await fetch(url)
